36d76c5528148046ffd1e50cd43c9922dc5b8308
Ian Duckworth <ianleeduckworth@gmail.com>
2017-02-27 19:26:31 -0500

    Adding file types necessary for multi-conditional search

---
+++ EazyE2E/Enums/PropertyType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EazyE2E.Enums
+{
+    public enum PropertyType
+    {
+        AutomationId,
+        Name,
+        Class,
+    }
+}
---
+++ EazyE2E/Helper/SearchTypeHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Automation;
+using EazyE2E.Enums;
+
+namespace EazyE2E.Helper
+{
+    public static class SearchTypeHelper
+    {
+        private static Dictionary<PropertyType, AutomationProperty> _backingDictionary;
+
+        public static AutomationProperty GetAutomationProperty(PropertyType type)
+        {
+            if (_backingDictionary == null) _backingDictionary = BuildBackingDictionary();
+            return _backingDictionary.FirstOrDefault(x => x.Key == type).Value;
+        }
+
+        private static Dictionary<PropertyType, AutomationProperty> BuildBackingDictionary()
+        {
+            var d = new Dictionary<PropertyType, AutomationProperty>
+            {
+                { PropertyType.AutomationId, AutomationElement.AutomationIdProperty},
+                { PropertyType.Name, AutomationElement.NameProperty},
+                { PropertyType.Class, AutomationElement.ClassNameProperty}
+            };
+
+            return d;
+        }
+
+
+    }
+}
