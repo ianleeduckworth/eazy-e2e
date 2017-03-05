a043aa51d53ca25d13c246edbf637a4a8f7bba45
Andrew Duncan <abduncan0@gmail.com>
2017-03-05 10:30:44 -0500

    Add start/stop capability to EzProcess

---
+++ EazyE2E.Console/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+using System.Threading;
+using System.Windows.Input;
+using EazyE2E.Element;
+using EazyE2E.HardwareManipulation;
+using EazyE2E.Process;
+
+namespace EazyE2E.Console
+{
+    internal class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            string calculatorPath = "C:\\Windows\\System32\\calc.exe";
+            using (var process = new EzProcess(calculatorPath, "Calculator"))
+            {
+                process.StartProcess();
+
+                //var root = new EzRoot("Calculator").RootElement;
+                var root = new EzRoot(process).RootElement;
+                var displayPane = root.FindDescendantByAutomationId("CalculatorResults");
+
+                //get the clear button
+                var clear = root.FindDescendantByAutomationId("clearButton");
+
+                //get parent elements for numbers and operators respectfully
+                var numbers = root.FindDescendantByAutomationId("NumberPad");
+                var standardOperators = root.FindDescendantByAutomationId("StandardOperators");
+
+                clear.Click();
+                EzKeyboardFunctions.PressKey(displayPane, Key.NumPad7);
+                var result = displayPane.Name.Split(' ')[2].Trim() == "7";
+                System.Console.WriteLine(result
+                    ? "Display pane is currently displaying 7"
+                    : $"There was an error.  Display pane is currenly displaying: ${displayPane.Name}");
+                Thread.Sleep(1000);
+                EzKeyboardFunctions.PressKey(displayPane, Key.Back);
+                result = displayPane.Name.Split(' ')[2].Trim() == "0";
+                System.Console.WriteLine(result
+                    ? "Display pane is currently displaying nothing, which is correct."
+                    : $"There ws an error.  Display pane is currently displaying: ${displayPane.Name}");
+
+                //get numbers buttons based on numbers object
+                //var oneButton = numbers.FindChildByAutomationId("num1Button");
+                //var twoButton = numbers.FindChildByAutomationId("num2Button");
+                //var fiveButton = numbers.FindChildByAutomationId("num5Button");
+                //var sevenButton = numbers.FindChildByAutomationId("num7Button");
+                //var nineButton = numbers.FindChildByAutomationId("num9Button");
+
+                //get operator buttons based on standardOperators object
+                //var plusButton = standardOperators.FindChildByAutomationId("plusButton");
+                //var minusButton = standardOperators.FindChildByAutomationId("minusButton");
+                //var multiplyButton = standardOperators.FindChildByAutomationId("multiplyButton");
+                //var equalsButton = standardOperators.FindChildByAutomationId("equalButton");
+
+                //click the buttons
+                //ExecWrapper(clear, "clear");
+                //ExecWrapper(sevenButton, "7");
+                //ExecWrapper(plusButton, "+");
+                //ExecWrapper(nineButton, "9");
+                //ExecWrapper(minusButton, "-");
+                //ExecWrapper(oneButton, "1");
+                //ExecWrapper(twoButton, "2");
+                //ExecWrapper(multiplyButton, "x");
+                //ExecWrapper(fiveButton, "5");
+                //ExecWrapper(equalsButton, "=");
+
+                //var result = displayPane.Name == "Display is 20";
+                //Console.WriteLine(result ? "Test passed.  Calculator output is 20" : $"Test failed.  Calculator output is {displayPane.Name.Split(' ')[2].Trim()} where it should have been 20");
+
+                System.Console.Write("Press Enter to exit");
+                System.Console.ReadLine();
+            }
+        }
+
+        private static void ExecWrapper(EzElement element, string outputKey)
+        {
+            element.Click();
+            System.Console.WriteLine($"Clicking {outputKey} element");
+            Thread.Sleep(1000);
+        }
+    }
+}
--- EazyE2E/Process/EzProcess.cs
+++ EazyE2E/Process/EzProcess.cs
@@ -1,42 +1,69 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
 
 namespace EazyE2E.Process
 {
-    public class EzProcess
+    public class EzProcess : IDisposable
     {
+        private readonly string _processFullPath;
         private readonly string _processName;
-        private readonly string _arguments;
-        private readonly ProcessWindowStyle _winStyle;
+        private System.Diagnostics.Process _process;
 
-        public EzProcess(string processName, string arguments = "", ProcessWindowStyle winStyle = ProcessWindowStyle.Normal)
+        public int ProcessId => _process.Id;
+        public string Arguments { get; set; } = string.Empty;
+        public ProcessWindowStyle WindowStyle { get; set; } = ProcessWindowStyle.Normal;
+
+        public EzProcess(string processFullPath, string processName)
         {
+            _processFullPath = processFullPath;
             _processName = processName;
-            _arguments = arguments;
-            _winStyle = winStyle;
         }
 
-        public int StartProcess()
+
+        public void StartProcess()
         {
             var start = new ProcessStartInfo
             {
-                Arguments = _arguments,
-                FileName = _processName,
-                WindowStyle = _winStyle
+                FileName = _processFullPath,
+                WindowStyle = WindowStyle,
+                Arguments = Arguments
             };
-            int exitCode;
 
-            using (System.Diagnostics.Process proc = System.Diagnostics.Process.Start(start))
-            {
-                if (proc == null) throw new NullReferenceException($"Could not find process where ProcessName = {_processName}, and Arguments = {_arguments}");
-                proc.Start(); //todo not sure if this is the right method call.  Want to go to the next line once the process is fully launched and ready to begin testing
-                exitCode = proc.ExitCode;
-            }
+            _process = System.Diagnostics.Process.Start(start);
 
-            return exitCode;
+            FindRunningProcess();
         }
 
-        public string ProcessName => _processName;
-        public string Arguments => _arguments;
+        /// <summary>
+        /// If a process is already started when calling Process.Start,
+        /// and the process looks for an existing process, sometimes
+        /// it will forward the request to the existing process and then
+        /// exit.  This method handles that scenario and gets a reference
+        /// to the correct process.
+        /// </summary>
+        private void FindRunningProcess()
+        {
+            // Give the process 1 second to start or forward the start request.
+            _process.WaitForExit(1000);
+
+            // If it hasn't exited, we've probably
+            // got the right process.
+            if (!_process.HasExited) return;
+
+            // If it has exited, find the currently running process.
+            var process = System.Diagnostics.Process.GetProcessesByName(_processName);
+            if (process.Length > 1) throw new InvalidOperationException($"There are more than one process with the name {_processName}.  Please close all running processes with that name.");
+            if (process.Length == 0) throw new InvalidOperationException($"A process with the name {_processName} could not be found.");
+            _process = process.Single();
+        }
+
+        public void Dispose()
+        {
+            _process.Kill();
+            _process.Dispose();
+        }
     }
 }
