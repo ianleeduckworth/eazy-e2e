f55b5cbaef555e41f9b32f533b5530165b4c5952
Ian Duckworth <ianleeduckworth@gmail.com>
2017-03-13 11:32:52 -0400

    Added code to check multiple memory metrics at once.  Also added a call to refresh the process before getting any of the memory metrics, otherwise they will appear to never change from the programmer's perspective.

--- EazyE2E/Performance/EzPerformanceMonitor.cs
+++ EazyE2E/Performance/EzPerformanceMonitor.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
 using EazyE2E.Process;
 
@@ -13,15 +14,86 @@ namespace EazyE2E.Performance
             _process = process;
         }
 
-        public long NonpagedSystemMemorySize => _process.Process.NonpagedSystemMemorySize64;
-        public long PagedMemorySize => _process.Process.PagedMemorySize64;
-        public long PagedSystemMemorySize => _process.Process.PagedSystemMemorySize64;
-        public long PeakPagedMemorySize => _process.Process.PeakPagedMemorySize64;
-        public long PeakVirtualMemorySize => _process.Process.PeakVirtualMemorySize64;
-        public long PeakWorkingSet => _process.Process.PeakWorkingSet64;
-        public long PrivateMemorySize => _process.Process.PrivateMemorySize64;
-        public long VirtualMemorySize => _process.Process.VirtualMemorySize64;
-        public long WorkingSet => _process.Process.WorkingSet64;
+        public long NonpagedSystemMemorySize
+        {
+            get
+            {
+                _process.Process.Refresh();
+                return _process.Process.NonpagedSystemMemorySize64;
+            }
+        }
+
+        public long PagedMemorySize
+        {
+            get
+            {
+                _process.Process.Refresh();
+                return _process.Process.PagedMemorySize64;
+            }
+        }
+
+        public long PagedSystemMemorySize
+        {
+            get
+            {
+                _process.Process.Refresh();
+                return _process.Process.PagedSystemMemorySize64;
+            }
+        }
+
+        public long PeakPagedMemorySize
+        {
+            get
+            {
+                _process.Process.Refresh();
+                return _process.Process.PeakPagedMemorySize64;
+            }  
+        }
+
+        public long PeakVirtualMemorySize
+        {
+            get
+            {
+                _process.Process.Refresh();
+                return _process.Process.PeakVirtualMemorySize64;
+            }  
+        }
+
+        public long PeakWorkingSet
+        {
+            get
+            {
+                _process.Process.Refresh();
+                return _process.Process.PeakWorkingSet64;
+            }  
+        }
+
+        public long PrivateMemorySize
+        {
+            get
+            {
+                _process.Process.Refresh();
+                return _process.Process.PrivateMemorySize64;
+            }
+        }
+
+        public long VirtualMemorySize
+        {
+            get
+            {
+                _process.Process.Refresh();
+                return _process.Process.VirtualMemorySize64;
+            }
+        }
+
+        public long WorkingSet
+        {
+            get
+            {
+                _process.Process.Refresh();
+                return _process.Process.WorkingSet64;
+            }
+        } 
 
         /// <summary>
         /// Begins a synchronous watch of a particular memory metric for a particular amount of time.  
@@ -32,7 +104,7 @@ namespace EazyE2E.Performance
         /// <param name="timeInSeconds">The amount of time IN SECONDS (not miliseconds) to profile for</param>
         /// <param name="amount">The threshold of failure; if the memory metric goes higher than this amount, the ifFail method will be called</param>
         /// <param name="ifFail">Action to do if a failure occurs.  Returns the memory type being profiled for, the original failure threshold, and the amount that caused the failure</param>
-        /// <param name="ifSuccess">Action to do if the profile succeeds.  Returns the memory type being profiled for and the original failure threshold</param>
+        /// <param name="ifSuccess">Action to do if the profile succeeds.  Returns the original memory type being profiled and the failure threshold</param>
         public void StartSyncWatch(MemoryType type, int timeInSeconds, long amount, Action<MemoryType, long, long> ifFail, Action<MemoryType, long> ifSuccess)
         {
             for (var i = 0; i < timeInSeconds; i++)
@@ -43,12 +115,39 @@ namespace EazyE2E.Performance
                     return;
                 }
 
-                Thread.Sleep(1000); //todo I don't really like Thread.sleep...
+                Thread.Sleep(1000); //todo I don't really like Thread.sleep... convert to stopwatch?
             }
 
             ifSuccess(type, amount);
         }
 
+        /// <summary>
+        /// Begins a synchronous watch of a particular memory metric for a particular amount of time.  
+        /// If any of the memory metrics selected go above the amount passed in, the ifFail method will be called.  
+        /// If, after the time has passed, all of the memory metrics have not exceeded the threshhold, the ifSuccess method will be called
+        /// </summary>
+        /// <param name="timeInSeconds">The amount of time IN SECONDS (not miliseconds) to profile for</param>
+        /// <param name="ifFail">Action to do if a failure occurs.  Returns the memory type being profiled for, the original failure threshold, and the amount that caused the failure</param>
+        /// <param name="ifSuccess">Action to do if the profile succeeds.  Returns the original array of MemoryWatches being profiled for.  Each MemoryWatch contains a MemoryType and a failure threshold</param>
+        /// <param name="watches">A list of items to watch.  Each item is composed of a memory type an an amount to watch for</param>
+        public void StartSyncWatch(int timeInSeconds, Action<MemoryType, long, long> ifFail, Action<MemoryWatch[]> ifSuccess, params MemoryWatch[] watches)
+        {
+            for (var i = 0; i < timeInSeconds; i++)
+            {
+                var failure = watches.FirstOrDefault(x => CheckMemory(x.Type, x.Amount));
+                if (failure != null)
+                {
+                    ifFail(failure.Type, failure.Amount, GetMemoryFromType(failure.Type));
+                    return;
+                }
+
+                Thread.Sleep(1000); //todo I don't really like Thread.sleep... convert to stopwatch?
+            }
+
+            ifSuccess(watches);
+        }
+
+        // Note that for this method, true = a failure,  false = a success for the memory type checked.
         private bool CheckMemory(MemoryType type, long amount)
         {
             switch (type)
---
+++ EazyE2E/Performance/MemoryWatch.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EazyE2E.Process;
+
+namespace EazyE2E.Performance
+{
+    public class MemoryWatch
+    {
+        public MemoryType Type { get; set; }
+        public long Amount { get; set; }
+    }
+}
