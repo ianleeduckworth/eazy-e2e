8d0d02e0b1a00503933def6b57129ca89b33e2f8
Ian Duckworth <ianleeduckworth@gmail.com>
2017-05-30 10:10:10 -0400

    Adding Configuration and Exceptions folders

---
+++ EazyE2E/Exceptions/UnauthorizedSearchException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EazyE2E.Exceptions
+{
+    public class UnauthorizedSearchException : Exception
+    {
+        public UnauthorizedSearchException(string message) : base(message)
+        {
+        }
+    }
+}
