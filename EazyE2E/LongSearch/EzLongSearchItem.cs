4c1a8b025c954dc0612da145c265d9c5b03ba7e4
Ian Duckworth <ianleeduckworth@gmail.com>
2017-11-06 20:39:55 -0500

    Added documentation

---
+++ EazyE2E/LongSearch/EzLongSearchItem.cs
@@ -0,0 +1,31 @@
+﻿using EazyE2E.Enums;
+
+namespace EazyE2E.LongSearch
+{
+    /// <summary>
+    /// Class EzLongSearchItem to be used with in the scope of EzLongSearch
+    /// </summary>
+    public class EzLongSearchItem
+    {
+        /// <summary>
+        /// Constructor for EzLongSearchItem.  Defines what property to search by and the name associated with the property
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="paramName"></param>
+        public EzLongSearchItem(PropertyType type, string paramName)
+        {
+            this.PropertyType = type;
+            this.ParamName = paramName;
+        }
+
+        /// <summary>
+        /// PropertyType to be searched by
+        /// </summary>
+        public PropertyType PropertyType { get; }
+
+        /// <summary>
+        /// Name of property being searched by
+        /// </summary>
+        public string ParamName { get; }
+    }
+}
