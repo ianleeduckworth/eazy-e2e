7936214e0202e176c1ea50d0fd4c6e87df0f8add
Ian Duckworth <ianleeduckworth@gmail.com>
2018-02-04 21:37:31 -0500

    Updated EzProcess class to include AttachToExistingProcess method

--- EazyE2E/Process/EzProcess.cs
+++ EazyE2E/Process/EzProcess.cs
@@ -3,21 +3,50 @@
 using System;
 using System.Diagnostics;
 using System.Linq;
+using System.Management;
+using System.Text;
 using EazyE2E.Configuration;
 
 namespace EazyE2E.Process
 {
+    /// <summary>
+    /// Houses all process related information and functionality.  Can be used to start a process or to query information about the currently running process
+    /// </summary>
     public class EzProcess : IDisposable
     {
         private readonly string _processFullPath;
         private readonly string _processName;
         private System.Diagnostics.Process _process;
+        private string _arguments;
 
+        /// <summary>
+        /// Underlying System.Diagnostics.Process instance
+        /// </summary>
         public System.Diagnostics.Process Process => _process;
+
+        /// <summary>
+        /// The underlying processId of the application as it would appear in task manager, for example
+        /// </summary>
         public int ProcessId => _process.Id;
+
+        /// <summary>
+        /// 	The path within the operating's folder structure to the exceutable that was launched to spawn this process
+        /// </summary>
         public string ProcessPath => _processFullPath;
+
+        /// <summary>
+        /// The name of the current process
+        /// </summary>
         public string ProcessName => _processName;
-        public string Arguments { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets the arguments that were used to launch the current process
+        /// </summary>
+        public string Arguments => _arguments ?? (_arguments = GetArguments());
+
+        /// <summary>
+        /// Gets the current style of the window, such as whether it's minimized or maximized
+        /// </summary>
         public ProcessWindowStyle WindowStyle => Config.DefaultWindowStyle;
 
         /// <summary>
@@ -32,17 +61,30 @@ namespace EazyE2E.Process
         }
 
         /// <summary>
+        /// Creates an EzProcess instance based on the System.Diagnostics.Process instance passed in
+        /// </summary>
+        /// <param name="process"></param>
+        public EzProcess(System.Diagnostics.Process process)
+        {
+            _process = process;
+            _processName = process.ProcessName;
+            _processFullPath = process.MainModule.FileName;
+        }
+
+        /// <summary>
         /// Starts the process based on the path given when instantiating EzProcess
         /// </summary>
-        public void StartProcess()
+        public void StartProcess(string arguments = null)
         {
+            if (_arguments == null) _arguments = arguments;
+
             TerminateExistingInstances();
 
             var start = new ProcessStartInfo
             {
                 FileName = _processFullPath,
                 WindowStyle = WindowStyle,
-                Arguments = Arguments,
+                Arguments = arguments,
                 RedirectStandardOutput = true,
                 RedirectStandardError = true,
                 UseShellExecute = false,
@@ -53,11 +95,45 @@ namespace EazyE2E.Process
             FindRunningProcess();
         }
 
+        /// <summary>
+        /// Attaches to an existing process based on the process name passed in
+        /// </summary>
+        /// <param name="processName"></param>
+        /// <returns></returns>
+        public static EzProcess AttachToExistingProcess(string processName)
+        {
+            return new EzProcess(GetProcess(processName));
+        }
+
+        private string GetArguments()
+        {
+            var commandLine = new StringBuilder(_process.MainModule.FileName);
+            commandLine.Append(" ");
+            using (var searcher = new ManagementObjectSearcher($"SELECT CommandLine FROM Win32_Process WHERE ProcessId = {_process.Id}"))
+            {
+                foreach (var result in searcher.Get())
+                {
+                    commandLine.Append(result["CommandLine"]);
+                    commandLine.Append(" ");
+                }
+            }
+
+            return commandLine.ToString();
+        }
+
         private void TerminateExistingInstances()
         {
             if (!Config.TerminateExistingInstance) return;
             var processes = System.Diagnostics.Process.GetProcessesByName(_processName).ToList();
             processes.ForEach(p => p.Kill());
+        }
+
+        private static System.Diagnostics.Process GetProcess(string processName)
+        {
+            var process = System.Diagnostics.Process.GetProcessesByName(processName);
+            if (process.Length > 1) throw new InvalidOperationException($"There are more than one process with the name {processName}.  Please close all running processes with that name.");
+            if (process.Length == 0) throw new InvalidOperationException($"A process with the name {processName} could not be found.");
+            return process.Single();
         }
 
         /// <summary>
@@ -77,11 +153,7 @@ namespace EazyE2E.Process
             if (!_process.HasExited) return;
 
             // If it has exited, find the currently running process.
-            var process = System.Diagnostics.Process.GetProcessesByName(_processName);
-            if (process.Length > 1) throw new InvalidOperationException($"There are more than one process with the name {_processName}.  Please close all running processes with that name.");
-            if (process.Length == 0) throw new InvalidOperationException($"A process with the name {_processName} could not be found.");
-
-            _process = process.Single();
+            _process = GetProcess(_processName);
 
             _process.StartInfo.RedirectStandardError = true;
             _process.StartInfo.RedirectStandardOutput = true;
