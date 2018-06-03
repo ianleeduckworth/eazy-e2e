2ffb49fdf56541a4993b0dce1fe08805430b6ad8
Ian Duckworth <ianleeduckworth@gmail.com>
2018-06-03 11:06:06 -0400

    Updated release notes

--- EazyE2E.Console/Program.cs
+++ EazyE2E.Console/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Threading;
+using System.Windows.Input;
 using EazyE2E.Element;
+using EazyE2E.HardwareManipulation;
 using EazyE2E.Process;
 
 namespace EazyE2E.Console
@@ -9,16 +12,19 @@ namespace EazyE2E.Console
         [STAThread]
         static void Main()
         {
-            const string appPath = "C:\\Windows\\System32\\calc.exe";
-            using (var process = new EzProcess(appPath, "Calculator"))
+            const string appPath = "C:\\Windows\\System32\\notepad.exe";
+            using (var process = new EzProcess(appPath, "Notepad"))
             {
                 process.StartProcess();
 
-                var root = new EzRoot(process);
-                var numberPad = root.RootElement.FindChildByAutomationId("NumberPad");
-                var sevenButton = numberPad.FindChildByAutomationId("num7Button");
+                var root = new EzRoot(process).RootElement;
+				var mainEditor = root.FindChildByName("Text Editor");
 
-                sevenButton.Click();
+				Thread.Sleep(2000);
+
+				EzMouseFunctions.LeftClick(mainEditor);
+				EzKeyboardFunctions.CtrlCombination(mainEditor, Key.O, Key.J, Key.F);
+
 
                 //pause
                 System.Console.ReadLine();
